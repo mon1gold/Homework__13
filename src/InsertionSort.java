1dfe1b9e78fcf908954e3ad9b457fdf74901deba
mon1gold <daniildocenko218@gmail.com>
2023-04-06 18:18:54 +0200

    add homework

---
+++ src/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public void insertionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = new int[]{123, 432, 1, 7, 23, 5, 43};
+        BublSort bublSort = new BublSort();
+        bublSort.bublSort(numbers);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.insertionSort(numbers);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
